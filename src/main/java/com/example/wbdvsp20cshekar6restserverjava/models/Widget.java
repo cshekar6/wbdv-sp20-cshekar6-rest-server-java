8da27dd76373fea9ae7ca58c1e8ec4aa7558b463
cshekar86 <gubbi.c@husky.neu.edu>
2020-02-20 12:40:25 -0500

    model and service for widget

---
+++ src/main/java/com/example/wbdvsp20cshekar6restserverjava/models/Widget.java
@@ -0,0 +1,143 @@
+package com.example.wbdvsp20cshekar6restserverjava.models;
+
+/**
+ * @author cshekar6
+ */
+public class Widget {
+  private String name;
+  private String id;
+  private String type = "HEADING";
+  private int order;
+  private String text;
+  private String url;
+  private int size;
+  private int width;
+  private int height;
+  private String cssClass;
+  private String style;
+  private String value;
+
+  private String title;
+  private String topicId;
+
+  public Widget(String id,String type) {
+    this.id = id;
+    this.type = type;
+  }
+
+  public Widget() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public int getOrder() {
+    return order;
+  }
+
+  public void setOrder(int order) {
+    this.order = order;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void setSize(int size) {
+    this.size = size;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
+  public String getCssClass() {
+    return cssClass;
+  }
+
+  public void setCssClass(String cssClass) {
+    this.cssClass = cssClass;
+  }
+
+  public String getStyle() {
+    return style;
+  }
+
+  public void setStyle(String style) {
+    this.style = style;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getTopicId() {
+    return topicId;
+  }
+
+  public void setTopicId(String topicId) {
+    this.topicId = topicId;
+  }
+
+}
